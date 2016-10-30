63dbd0f710301c6ab5400ed1d32e1a7e5af3bf9f
Arthur Chang <arthurc.iphone@gmail.com>
2016-10-30 21:41:06 +0800

    add fix 4.8

---
+++ Linux/Arch/vmware_workstation/fix_vm_for_linux_4.8.sh
@@ -0,0 +1,14 @@
+cd /usr/lib/vmware/modules/source
+tar xf vmmon.tar
+mv vmmon.tar vmmon.old.tar
+sed -r -i -e 's/get_user_pages(_remote)*/get_user_pages_remote/g' vmmon-only/linux/hostif.c
+sed -r -i -e 's/NR_ANON_PAGES/NR_ANON_MAPPED/g' vmmon-only/linux/hostif.c
+tar cf vmmon.tar vmmon-only
+rm -r vmmon-only
+tar xf vmnet.tar
+mv vmnet.tar vmnet.old.tar
+sed -r -i -e 's/get_user_pages(_remote)*/get_user_pages_remote/g' vmnet-only/userif.c
+sed -i -e 's/dev->trans_start = jiffies/netif_trans_update\(dev\)/g' vmnet-only/netif.c
+tar cf vmnet.tar vmnet-only
+rm -r vmnet-only
+echo "done"
